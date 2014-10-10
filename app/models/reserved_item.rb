7fb349815e736a720ca50ead901819fef9336434
Obfuscoder <obfuscoder@obfusco.de>
2014-10-10 18:19:12 +0200

    Update all controller specs to use shared examples

--- app/models/reserved_item.rb
+++ app/models/reserved_item.rb
@@ -3,6 +3,7 @@ class ReservedItem < ActiveRecord::Base
   belongs_to :item
 
   validates_presence_of :reservation, :item, :number, :code
+  validates :number, numericality: { greater_than: 0 }
 
   def to_s
     code
