113a5bbaf1610ccf5febf57cc5e0fadb9a84b45e
Obfuscoder <obfuscoder@obfusco.de>
2015-06-24 22:41:25 +0200

    Use sendmail for actionmailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'test.flohmarkthelfer.de' }
+  config.action_mailer.delivery_method = :sendmail
 end
