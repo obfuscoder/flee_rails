7e7a9c9d23558282dc14e6e1c1c09eec3cb48f8d
Obfuscoder <obfuscoder@obfusco.de>
2015-07-19 23:11:22 +0200

    Capture important areas for presentations

--- spec/features/screenshots.rb
+++ spec/features/screenshots.rb
@@ -3,10 +3,10 @@ require 'features/admin/login'
 require 'rmagick'
 
 RSpec.describe 'Screenshots', js: true do
-  def capture(name)
+  def capture(name, options = {})
     sleep 1
     file_path = "screenshots/#{name}.png"
-    save_screenshot file_path
+    save_screenshot file_path, options
     puts "Captured #{file_path}"
   end
 
@@ -27,11 +27,12 @@ RSpec.describe 'Screenshots', js: true do
     visit admin_path
     fill_in 'eMail-Adresse', with: admin.email
     fill_in 'Passwort', with: 'password'
-    capture :admin_login
+    capture :admin_login, selector: 'div[role="main"]'
 
     click_on 'Anmelden'
     click_on 'Termine'
-    capture :admin_events
+    capture :admin_events_menu, selector: 'div.navbar'
+    capture :admin_events, selector: 'div[role="main"]'
 
     click_on 'Neuer Termin'
     fill_in 'Name', with: 'Kinderflohmarkt'
@@ -40,49 +41,52 @@ RSpec.describe 'Screenshots', js: true do
     fill_in 'maximale Anzahl Artikel je Verkäufer', with: 50
     fill_in 'Reservierungsstart', with: I18n.localize(2.days.ago, format: :short)
     find('#event_shopping_start').click
-    capture :admin_event_new
+    capture :admin_event_new, selector: 'div[role="main"]'
 
     click_on 'Termin erstellen'
-    capture :admin_event_created
+    capture :admin_event_created, selector: 'div[role="main"]'
     event = Event.last
 
     click_on 'Anzeigen'
-    capture :admin_event_show
+    capture :admin_event_show, selector: 'div[role="main"]'
 
     click_on 'Reservierungen'
-    capture :admin_event_reservations
+    capture :admin_event_reservations, selector: 'div[role="main"]'
 
     click_on 'Neue Reservierung'
 
     sellers.take(15).each { |seller| check seller.label_for_reservation }
-    capture :admin_event_reservation_new
+    capture :admin_event_reservation_new, selector: 'div[role="main"]'
 
     click_on 'Reservierung erstellen'
-    capture :admin_event_reservation_created
+    capture :admin_event_reservation_created, selector: 'div[role="main"]'
 
     click_on 'Verkäufer'
-    capture :admin_sellers
+    capture :admin_sellers_menu, selector: 'div.navbar'
+    capture :admin_sellers, selector: 'div[role="main"]'
 
     find_link('Anzeigen', href: admin_seller_path(sellers.first)).click
-    capture :admin_seller_show
+    capture :admin_seller_show, selector: 'div[role="main"]'
 
     click_on 'Bearbeiten'
-    capture :admin_seller_edit
+    capture :admin_seller_edit, selector: 'div[role="main"]'
 
     click_on 'Änderungen speichern'
 
     %w(Hose Jacke Puzzle Schuhe Fahhrad Autositz).each { |name| FactoryGirl.create :category, name: name }
     click_on 'Kategorien'
-    capture :admin_categories
+    capture :admin_categories_menu, selector: 'div.navbar'
+    capture :admin_categories, selector: 'div[role="main"]'
 
     click_on 'Neue Kategorie'
     fill_in 'Name', with: 'Spielzeug'
-    capture :admin_category_new
+    capture :admin_category_new, selector: 'div[role="main"]'
 
     click_on 'Kategorie erstellen'
-    capture :admin_category_created
+    capture :admin_category_created, selector: 'div[role="main"]'
 
     click_on 'Mails'
+    capture :admin_mails_menu, selector: 'div.navbar'
     fill_in 'Betreff', with: 'Helfer für unseren kommenden Flohmarkt gesucht'
     fill_in 'Inhalt', with: <<-END
 Sehr geehrte Verkäuferin/sehr geehrter Verkäufer
@@ -103,10 +107,10 @@ END
     within '.email_reservation' do
       choose 'Ja'
     end
-    capture :admin_email
+    capture :admin_email, selector: 'div[role="main"]'
 
     click_on '15 ausgewählt'
-    capture :admin_email_select
+    capture :admin_email_select, selector: 'div[role="main"]'
 
     find('#admin-info > a').click
     click_on 'Abmelden'
@@ -126,47 +130,48 @@ END
     fill_in 'eMail-Adresse', with: email
     fill_in 'Telefonnummer', with: '0815/4711-1234'
     check 'Ich akzeptiere die Teilnahmebedingungen und Datenschutzerklärung.'
-    capture :registration
+    capture :registration, selector: 'div[role="main"]'
 
     click_on 'Registrieren'
-    capture :registration_done
+    capture :registration_done, selector: 'div[role="main"]'
 
     seller = Seller.find_by_email(email)
 
     visit login_seller_path(seller.token)
-    capture :seller_home
+    capture :seller_home, selector: 'div[role="main"]'
 
     click_on 'Stammdaten bearbeiten'
-    capture :seller_edit
+    capture :seller_edit, selector: 'div[role="main"]'
 
     click_on 'Zurück ohne Speichern der Änderungen'
     click_on 'einen Verkäuferplatz reservieren'
-    capture :seller_reservation_created
+    capture :seller_reservation_created, selector: 'div[role="main"]'
 
     reservation = Reservation.last
 
     click_on 'Artikel bearbeiten'
-    capture :seller_items
+    capture :seller_items, selector: 'div[role="main"]'
+
 
     create_item category: 'Schuhe', description: 'pink mit Streifen', size: 12, price: '4,00' do
-      capture :seller_item_new
+      capture :seller_item_new, selector: 'div[role="main"]'
     end
-    capture :seller_item_created
+    capture :seller_item_created, selector: 'div[role="main"]'
 
     Category.all.each do |category|
       FactoryGirl.create_list :random_item, 3, category: category, reservation: reservation
     end
     visit event_items_path(event)
-    capture :seller_items_created
+    capture :seller_items_created, selector: 'div[role="main"]'
 
     click_on 'Etiketten drucken'
     click_on 'ausgewählt'
-    capture :seller_items_print
+    capture :seller_items_print, selector: 'div[role="main"]'
 
     event.update shopping_end: 1.hour.ago
 
     visit seller_path
-    capture :seller_home_with_event_finished
+    capture :seller_home_with_event_finished, selector: 'div[role="main"]'
 
     click_on 'Flohmarkt bewerten'
     within('.review_registration') { choose '2' }
@@ -175,7 +180,7 @@ END
     choose 'Ja'
     fill_in 'Welche Dinge haben Ihnen nicht gefallen', with: 'Mehr Farben'
 
-    capture :seller_review
+    capture :seller_review, selector: 'div[role="main"]'
     click_on 'Bewertung abschließen'
 
     event.reservations.take(10).each do |reservation|
@@ -187,15 +192,16 @@ END
     fill_in 'Passwort', with: 'password'
     click_on 'Anmelden'
     sleep 3
-    capture :admin_home
+    capture :admin_home_menu, selector: 'div.navbar'
+    capture :admin_home, selector: 'div[role="main"]'
 
     find('#admin-info > a').click
     capture :admin_info
 
     click_on 'Passwort ändern'
-    capture :admin_change_password
+    capture :admin_change_password, selector: 'div[role="main"]'
 
     visit admin_event_reviews_path(event)
-    capture :admin_reviews
+    capture :admin_reviews, selector: 'div[role="main"]'
   end
 end
