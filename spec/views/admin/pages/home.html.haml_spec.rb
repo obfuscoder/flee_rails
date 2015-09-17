5d04c663df80931c8c1acdc573c2c8ab40fcdf69
Obfuscoder <obfuscoder@obfusco.de>
2015-09-17 20:47:39 +0200

    Add link to cashier system download

---
+++ spec/views/admin/pages/home.html.haml_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'admin/pages/home' do
+  it_behaves_like 'a standard view'
+
+  it 'provides link to cashier system software' do
+    render
+    expect(rendered).to have_link 'Download Kassensystem'
+  end
+end
