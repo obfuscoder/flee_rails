fdbf1843b6e98a48f67f585602cffa7ecf763efa
Obfuscoder <obfuscoder@obfusco.de>
2015-12-23 12:37:31 +0100

    Add validation for time period max to be later than min
    Close #41

--- app/models/time_period.rb
+++ app/models/time_period.rb
@@ -1,3 +1,11 @@
 class TimePeriod < ActiveRecord::Base
   belongs_to :event
+
+  validate :min_before_max
+
+  private
+
+  def min_before_max
+    errors.add :max, :before_min if max < min
+  end
 end
