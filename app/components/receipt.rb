18e1c0032b222c5f0b266490c8727cd6291b240d
Obfuscoder <obfuscoder@obfusco.de>
2016-06-11 20:19:51 +0200

    Use individual reservation fees and commission rates in receipts

--- app/components/receipt.rb
+++ app/components/receipt.rb
@@ -3,9 +3,9 @@ class Receipt
     @reservation = reservation
 
     commission_cut_sum =
-      (sold_items_sum * (1 - reservation.event.commission_rate) / reservation.event.price_precision).floor *
+      (sold_items_sum * (1 - reservation.commission_rate) / reservation.event.price_precision).floor *
         reservation.event.price_precision
-    @payout = commission_cut_sum - event.seller_fee
+    @payout = commission_cut_sum - reservation.fee
     @commission_cut = commission_cut_sum - sold_items_sum
   end
 
@@ -44,6 +44,6 @@ class Receipt
   end
 
   def seller_fee
-    - @reservation.event.seller_fee
+    - @reservation.fee
   end
 end
