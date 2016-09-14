11d46774591a956dab7bc079908a3d73b1215daa
Obfuscoder <obfuscoder@obfusco.de>
2016-09-14 20:21:00 +0200

    Log host name instead of domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [:domain]
+  config.log_tags = [:host]
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
