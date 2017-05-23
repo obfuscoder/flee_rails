ffbfe2b3430e7c04fff3ae5c14ca0323db878a6a
Obfuscoder <obfuscoder@obfusco.de>
2017-05-23 08:47:02 +0200

    Update capistrano deploy script for puma init.d integration

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,15 +16,16 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml',
                                                  'config/settings/brands/demo.local.yml',
                                                  'config/settings/brands/default.local.yml')
 
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/cache', 'backup', 'public/download', 'public/docs')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp', 'backup', 'public/download', 'public/docs')
 
 set :puma_threads, [4, 16]
 set :puma_workers, 2
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_state,      "#{shared_path}/tmp/puma/state"
+set :puma_pid,        "#{shared_path}/tmp/puma/pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :puma_preload_app, true
+set :activate_control_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 
@@ -38,8 +39,7 @@ namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
     on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
+      execute "mkdir #{shared_path}/tmp/puma -p"
     end
   end
 
