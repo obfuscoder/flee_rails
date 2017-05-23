d1f8a38da5640636b334215326e57d9b9f848668
Obfuscoder <obfuscoder@obfusco.de>
2017-05-23 08:58:53 +0200

    Update some puma config

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,8 +22,9 @@ set :puma_threads, [4, 16]
 set :puma_workers, 2
 set :puma_state,      "#{shared_path}/tmp/puma/state"
 set :puma_pid,        "#{shared_path}/tmp/puma/pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_access_log, "#{shared_path}/log/puma.error.log"
+set :puma_error_log,  "#{shared_path}/log/puma.access.log"
+set :puma_bind, 'tcp://127.0.0.1:9292'
 set :puma_preload_app, true
 set :activate_control_app, true
 set :puma_worker_timeout, nil
