8813bc1559e04ab586b9493a472baac50af120db
Obfuscoder <obfuscoder@obfusco.de>
2017-05-29 18:19:59 +0200

    Update references to pids in deployment script

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,10 +20,6 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp', 'backup', 'public/d
 
 set :puma_threads, [4, 16]
 set :puma_workers, 2
-set :puma_state,      "#{shared_path}/tmp/puma/state"
-set :puma_pid,        "#{shared_path}/tmp/puma/pid"
-set :puma_access_log, "#{shared_path}/log/puma.error.log"
-set :puma_error_log,  "#{shared_path}/log/puma.access.log"
 set :puma_bind, 'tcp://127.0.0.1:9292'
 set :puma_preload_app, true
 set :activate_control_app, true
@@ -44,5 +40,5 @@ namespace :puma do
     end
   end
 
-  before :start, :make_dirs
+  # before :start, :make_dirs
 end
