d98a474bd2ec6f1d2d890e0fc0837105788cc8e6
Obfuscoder <obfuscoder@obfusco.de>
2018-01-26 15:05:36 +0100

    Update dependencies

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@
 
 source 'https://rubygems.org'
 
-gem 'cancancan'
 gem 'coffee-rails'
 gem 'jquery-rails'
 gem 'jquery-tablesorter'
--- app/models/ability.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Ability
-  include CanCan::Ability
-
-  def initialize(user)
-    can :manage, Event, client_id: user.client_id
-  end
-end
