c4a3d50492fd3a2462d13be70963cbfe8105e989
Obfuscoder <obfuscoder@obfusco.de>
2018-02-07 18:34:28 +0100

    Destroy all notifications for a seller and event when creating reservation
    Close #205

--- app/services/create_reservation.rb
+++ app/services/create_reservation.rb
@@ -4,8 +4,20 @@ class CreateReservation
   def initialize; end
 
   def create(event, seller, save_options = {})
-    reservation = Reservation.new event: event, seller: seller
-    reservation.save(save_options) && SellerMailer.reservation(reservation).deliver_later
-    reservation
+    ActiveRecord::Base.transaction do
+      reservation = Reservation.new event: event, seller: seller
+      reservation.save(save_options) && send_reservation_mail(reservation) && destroy_notifications(event, seller)
+      reservation
+    end
+  end
+
+  private
+
+  def destroy_notifications(event, seller)
+    event.notifications.where(seller: seller).destroy_all
+  end
+
+  def send_reservation_mail(reservation)
+    SellerMailer.reservation(reservation).deliver_later
   end
 end
--- spec/services/create_reservation_spec.rb
+++ spec/services/create_reservation_spec.rb
@@ -5,18 +5,24 @@ require 'rails_helper'
 RSpec.describe CreateReservation do
   subject(:instance) { described_class.new }
   describe '#create' do
-    let(:event) { double :event }
+    subject(:action) { instance.create event, seller }
+
+    let(:event) { double :event, notifications: notifications }
     let(:seller) { double :seller }
-    let(:reservation) { double save: true }
+    let(:reservation) { double :reservation, save: true }
+    let(:notifications) { double :notification, where: relevant_notifications }
+    let(:relevant_notifications) { double :relevant_notifications, destroy_all: nil }
     let(:options) { {} }
     before do
-      expect(Reservation).to receive(:new).with(hash_including(event: event, seller: seller)).and_return(reservation)
+      allow(Reservation).to receive(:new).and_return(reservation)
       allow(SellerMailer).to receive(:reservation).and_return double(deliver_later: true)
     end
-    subject(:action) { instance.create event, seller }
-    it 'returns the reservation' do
-      expect(action).to eq reservation
+    it 'creates a reservation' do
+      action
+      expect(Reservation).to have_received(:new).with(hash_including(event: event, seller: seller))
     end
+
+    it { is_expected.to eq reservation }
 
     context 'when reservation was persisted' do
       it 'sends reservation confirmation mail' do
@@ -30,9 +36,16 @@ RSpec.describe CreateReservation do
     context 'when reservation was not persisted' do
       let(:reservation) { double save: false }
       it 'does not send reservation confirmation mail' do
-        expect(SellerMailer).not_to receive(:reservation)
         action
+        expect(SellerMailer).not_to have_received(:reservation)
       end
+    end
+
+    it 'removes all notifications for this event and seller' do
+      action
+      expect(event).to have_received(:notifications)
+      expect(notifications).to have_received(:where).with(seller: seller)
+      expect(relevant_notifications).to have_received(:destroy_all)
     end
 
     it 'passes on provided options to save' do
