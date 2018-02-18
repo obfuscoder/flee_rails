9e4037bbf73e67395fac882e593882b46e1e2705
Obfuscoder <obfuscoder@obfusco.de>
2018-02-18 15:25:58 +0100

    Fix broken spec

--- spec/views/admin/pages/home.html.haml_spec.rb
+++ spec/views/admin/pages/home.html.haml_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'admin/pages/home' do
 
   it 'provides link to cashier system software' do
     render
-    expect(rendered).to have_link 'Download Kassensystem'
+    expect(rendered).to have_link 'Kassensystem (Windows)'
+    expect(rendered).to have_link 'Mobiles Kassensystem (Android)'
   end
 end
