4b8fdf0bad007284f1a14f807baed944e9cb9d78
Obfuscoder <obfuscoder@obfusco.de>
2018-03-12 21:12:21 +0100

    Fix migration script for sent_count column in messages

--- db/migrate/20180309153115_uodate_count_columns_in_messages.rb
+++ db/migrate/20180309153115_uodate_count_columns_in_messages.rb
@@ -3,6 +3,6 @@
 class UodateCountColumnsInMessages < ActiveRecord::Migration
   def change
     rename_column :messages, :count, :scheduled_count
-    add_column :messages, :sent_count, :interger
+    add_column :messages, :sent_count, :integer
   end
 end
