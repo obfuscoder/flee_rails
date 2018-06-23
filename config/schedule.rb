236f008687f998eb13ecdb84aba25e21ce3a793e
Obfuscoder <obfuscoder@obfusco.de>
2018-06-23 15:11:16 +0200

    Fix schedule of db reset to 1 am

--- config/schedule.rb
+++ config/schedule.rb
@@ -10,6 +10,6 @@ every :day, at: '0:00' do
   command '/usr/sbin/logrotate -s ~/www/shared/tmp/logrotate.state ~/www/current/config/logrotate.conf > /dev/null 2>&1'
 end
 
-every :day, at: '1:00' do
+every :day, at: '1am' do
   rake 'db:demo:reset'
 end
