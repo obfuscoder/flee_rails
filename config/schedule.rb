2891471f637cc030594bb667cbeba75ea957e766
Obfuscoder <obfuscoder@obfusco.de>
2018-06-23 14:42:41 +0200

    Add daily execution of logrotate to rotate all logs
    Close #233

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,8 +2,12 @@
 
 set :output, 'log/cron.log'
 
-every :hour do
+every 4.hours do
   rake 'db:backup'
+end
+
+every :day, at: '0:00' do
+  command '/usr/sbin/logrotate -s ~/www/shared/tmp/logrotate.state ~/www/current/config/logrotate.conf > /dev/null 2>&1'
 end
 
 every :day, at: '1:00' do
