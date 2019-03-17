56e239007c7d398935e0d6aa913378f9c10ae6ba
Obfuscoder <obfuscoder@obfusco.de>
2019-03-17 13:21:16 +0100

    Show default reservation number form field conditionally

--- spec/views/admin/sellers/_form.html.haml_spec.rb
+++ spec/views/admin/sellers/_form.html.haml_spec.rb
@@ -4,8 +4,12 @@ require 'rails_helper'
 
 RSpec.describe 'admin/sellers/_form' do
   let(:seller) { build :seller }
+  let(:preparations) {}
 
-  before { assign :seller, seller }
+  before do
+    preparations
+    assign :seller, seller
+  end
 
   it_behaves_like 'a standard partial'
 
@@ -21,6 +25,12 @@ RSpec.describe 'admin/sellers/_form' do
     it { is_expected.to have_field 'seller_city' }
     it { is_expected.to have_field 'seller_phone' }
     it { is_expected.to have_field 'seller_email' }
-    it { is_expected.to have_field 'seller_default_reservation_number' }
+    it { is_expected.not_to have_field 'seller_default_reservation_number' }
+
+    context 'when Client#auto_reservation_numbers_start is configured' do
+      let(:preparations) { Client.first.update! auto_reservation_numbers_start: 100 }
+
+      it { is_expected.to have_field 'seller_default_reservation_number' }
+    end
   end
 end
