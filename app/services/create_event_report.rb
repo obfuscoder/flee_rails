4e8c62ab3863898b2797aba2a13966a19af6712a
Obfuscoder <obfuscoder@obfusco.de>
2019-06-08 13:16:03 +0200

    Add gender to event report

--- app/services/create_event_report.rb
+++ app/services/create_event_report.rb
@@ -7,9 +7,16 @@ class CreateEventReport
 
   def call
     items = @event.items.map do |item|
-      [item.reservation.number, item.number, item.category.name, item.description, item.size, item.price].join("\t")
+      [
+        item.reservation.number,
+        item.number,
+        item.category.name,
+        item.description,
+        item.size,
+        I18n.t('gender.' + item.gender),
+        item.price].join("\t")
     end
-    header = %w[Reservierungsnummer Artikelnummer Kategorie Beschreibung Größe Preis].join("\t")
+    header = %w[Reservierungsnummer Artikelnummer Kategorie Beschreibung Größe Geschlecht Preis].join("\t")
     ([header] + items).join("\n")
   end
 end
--- spec/services/create_event_report_spec.rb
+++ spec/services/create_event_report_spec.rb
@@ -6,9 +6,13 @@ RSpec.describe CreateEventReport do
   subject(:instance) { described_class.new event }
 
   let(:event) { create :event_with_ongoing_reservation }
-  let!(:categories) { create_list :category, 10 }
+  let!(:categories) { create_list :category, 10, gender: true }
   let!(:reservations) { create_list :reservation, 5, event: event }
-  let!(:items) { (0..49).map { create :item, reservation: reservations.sample, category: categories.sample } }
+  let!(:items) do
+    (0..49).map do
+      create :item, reservation: reservations.sample, category: categories.sample, gender: Item.genders.keys.sample
+    end
+  end
 
   describe '#call' do
     subject(:action) { instance.call }
@@ -20,7 +24,7 @@ RSpec.describe CreateEventReport do
       subject(:first_line) { action.lines.first }
 
       it 'contains columns separated with tabs' do
-        expect(first_line.split(/\t/)).to have(6).elements
+        expect(first_line.split(/\t/)).to have(7).elements
       end
     end
   end
