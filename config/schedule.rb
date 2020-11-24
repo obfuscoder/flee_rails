d9437cb7b1810674c5c3e389deef062a741c89ab
Obfuscoder <obfuscoder@obfusco.de>
2020-11-24 10:31:08 +0100

    Make db backup only once per day

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,10 @@
 set :output, 'log/cron.log'
 
-every 4.hours do
-  rake 'db:backup'
-end
-
 every :day, at: '0:00' do
   command '/usr/sbin/logrotate -s ~/www/shared/tmp/logrotate.state ~/www/current/config/logrotate.conf > /dev/null 2>&1'
 end
 
 every :day, at: '1am' do
   rake 'db:demo:reset'
+  rake 'db:backup'
 end
