24db28d5a4b91bc88cd492ee406c1914165d62c9
Obfuscoder <obfuscoder@obfusco.de>
2022-07-30 00:25:57 +0200

    Restart after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml',
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp', 'backup', 'public/download', 'public/docs')
 
+set :puma_phased_restart, true
 set :puma_conf, -> { "#{shared_path}/config/puma.rb" }
 set :puma_threads, [1, 1]
 set :puma_workers, 4
@@ -17,3 +18,5 @@ set :rvm_ruby_string, :local
 set :rollbar_token, '720d6e5df892434d93f421801f1a9f82'
 set(:rollbar_env, proc { fetch :stage })
 set(:rollbar_role, proc { :app })
+
+after 'deploy:publishing', 'deploy:restart'
